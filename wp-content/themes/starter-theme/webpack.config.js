5eae4d2feedde1598d4bfd88580342c6a58b8ac2
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-01-26 09:42:14 -0800

    Adding postcss config stuff

--- wp-content/themes/starter-theme/webpack.config.js
+++ wp-content/themes/starter-theme/webpack.config.js
@@ -1,20 +1,51 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 const config = {
   context: `${__dirname}/src`,
-  entry: './js/main.js',
+  entry: {
+    bundle: './js/main.js'
+  },
   output: {
     path: `${__dirname}/dist`,
-    filename: './js/bundle.js',
+    filename: './js/[name].js',
+    publicPath: 'https://starter-template.lndo.site/wp-content/themes/starter-theme/'
   },
   module: {
     rules: [{
       test: /\.css$/,
-      use: [
-        'style-loader',
-        { loader: 'css-loader', options: { importLoaders: 1 } },
-        'postcss-loader',
-      ],
-    }],
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['postcss-loader']
+      })
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=static/fonts/[name].[ext]' },
+    { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=static/fonts/[name].[ext]' },
+    { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=static/fonts/[name].[ext]' }
+    ]
   },
+  plugins: [
+    new ExtractTextPlugin('/css/[name].css'),
+    new BrowserSyncPlugin({
+      host: 'starter-template.lndo.site',
+      port: 3000,
+      proxy: 'starter-template.lndo.site:80',
+      files: [
+        '**/*.php',
+        '**/*.twig'
+      ]
+    })
+  ]
 };
 
 module.exports = config;
