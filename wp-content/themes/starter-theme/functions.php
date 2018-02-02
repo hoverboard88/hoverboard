2c21f4ec9c96de98bd393ea26051bde5d5bc5949
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-02-02 15:02:41 -0800

    adding favicons, etc

--- wp-content/themes/starter-theme/functions.php
+++ wp-content/themes/starter-theme/functions.php
@@ -17,6 +17,9 @@ Timber::$dirname = array('templates', 'src/views');
 class StarterSite extends TimberSite {
 
   function __construct() {
+
+    $this->theme_uri = get_template_directory_uri();
+
     add_theme_support( 'post-formats' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'menus' );
@@ -26,6 +29,7 @@ class StarterSite extends TimberSite {
     add_filter( 'upload_mimes', array(&$this, 'svg_support' ));
     add_action( 'init', array( $this, 'register_post_types' ) );
     add_action( 'init', array( $this, 'register_taxonomies' ) );
+    add_action( 'wp_head', array( &$this, 'wp_head' ) );
     add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );parent::__construct();
     $this->register_image_sizes();
   }
@@ -49,6 +53,14 @@ class StarterSite extends TimberSite {
 		// add_image_size( 'large', 1440 );
 	}
 
+  public function wp_head() {
+    $this->favicons();
+  }
+
+  private function favicons() { ?>
+    <!-- Favicon HTML -->
+  <?php }
+
   function enqueue_scripts_styles() {
     wp_enqueue_style( 'hb_bundle', get_template_directory_uri() . '/dist/css/bundle.css', false, filemtime( get_stylesheet_directory() . '/dist/css/bundle.css' ));
     wp_enqueue_script( 'hb_bundle', get_template_directory_uri() . '/dist/js/bundle.js', false, filemtime( get_stylesheet_directory() . '/dist/js/bundle.js' ));
@@ -57,9 +69,15 @@ class StarterSite extends TimberSite {
   // Add variables to templates
   function add_to_context( $context ) {
     // $context['foo'] = 'These values are available everytime you call Timber::get_context();';
+    $context['template_url'] = $this->theme_uri;
     $context['menu'] = new TimberMenu();
+    $context['options'] = get_fields('options'); // Get all global options
     $context['site'] = $this;
     return $context;
+  }
+
+  function svg( $filename ) {
+    return file_get_contents(get_stylesheet_directory() . '/src/img/' . $filename . '.svg');
   }
 
   function myfoo( $text ) {
@@ -70,6 +88,7 @@ class StarterSite extends TimberSite {
   function add_to_twig( $twig ) {
     /* this is where you can add your own functions to twig */
     $twig->addExtension( new Twig_Extension_StringLoader() );
+    $twig->addFilter('svg', new Twig_SimpleFilter('svg', array($this, 'svg')));
     // See myfoo above
     // $twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
     return $twig;
