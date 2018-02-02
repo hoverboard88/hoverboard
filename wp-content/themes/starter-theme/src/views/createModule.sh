7a904dd1ed0e1b94d6c6977c3819a12976d715dc
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-02-02 14:04:48 -0800

    fixing css extension

--- wp-content/themes/starter-theme/src/views/createModule.sh
+++ wp-content/themes/starter-theme/src/views/createModule.sh
@@ -5,4 +5,4 @@ printf "Creating Module: \"${1}\"";
 mkdir ${1}
 touch ${1}/${1}.css
 touch ${1}/${1}.twig
-printf "@import \"../views/${1}/${1}.css\";\n" >> ../src/css/main.scss
+printf "@import \"../views/${1}/${1}.css\";\n" >> ../css/main.css
