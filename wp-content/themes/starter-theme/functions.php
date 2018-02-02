78d1fdc1cef253c28779b5ac304fc6fd3bc4ab96
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-02-02 14:51:39 -0800

    Adding various things to functions

--- wp-content/themes/starter-theme/functions.php
+++ wp-content/themes/starter-theme/functions.php
@@ -23,9 +23,11 @@ class StarterSite extends TimberSite {
     add_theme_support( 'html5', array( 'search-form', 'gallery', 'caption' ) );
     add_filter( 'timber_context', array( $this, 'add_to_context' ) );
     add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+    add_filter( 'upload_mimes', array(&$this, 'svg_support' ));
     add_action( 'init', array( $this, 'register_post_types' ) );
     add_action( 'init', array( $this, 'register_taxonomies' ) );
     add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );parent::__construct();
+    $this->register_image_sizes();
   }
 
   function register_post_types() {
@@ -36,15 +38,25 @@ class StarterSite extends TimberSite {
     //this is where you can register custom taxonomies
   }
 
+  function add_svg_support( $mimes ) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+
+  // Add extra image sizes here
+	public function register_image_sizes() {
+		// EX:
+		// add_image_size( 'large', 1440 );
+	}
+
   function enqueue_scripts_styles() {
     wp_enqueue_style( 'hb_bundle', get_template_directory_uri() . '/dist/css/bundle.css', false, filemtime( get_stylesheet_directory() . '/dist/css/bundle.css' ));
     wp_enqueue_script( 'hb_bundle', get_template_directory_uri() . '/dist/js/bundle.js', false, filemtime( get_stylesheet_directory() . '/dist/js/bundle.js' ));
   }
 
+  // Add variables to templates
   function add_to_context( $context ) {
-    $context['foo'] = 'bar';
-    $context['stuff'] = 'I am a value set in your functions.php file';
-    $context['notes'] = 'These values are available everytime you call Timber::get_context();';
+    // $context['foo'] = 'These values are available everytime you call Timber::get_context();';
     $context['menu'] = new TimberMenu();
     $context['site'] = $this;
     return $context;
@@ -58,7 +70,8 @@ class StarterSite extends TimberSite {
   function add_to_twig( $twig ) {
     /* this is where you can add your own functions to twig */
     $twig->addExtension( new Twig_Extension_StringLoader() );
-    $twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
+    // See myfoo above
+    // $twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
     return $twig;
   }
 
