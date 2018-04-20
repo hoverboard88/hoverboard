617fc742a023243517b0224cf711affc9131f00f
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-04-20 10:54:02 -0700

    Making dev versions of compiled files

---
+++ wp-content/themes/starter-theme/webpack.dev.js
@@ -0,0 +1,21 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  entry: {
+    dev: './js/main.js'
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'https://hoverboardtheme.lndo.site',
+      port: 3000,
+      proxy: 'https://hoverboardtheme.lndo.site',
+      files: [
+        '**/*.php',
+        '**/*.twig'
+      ]
+    })
+  ]
+})
