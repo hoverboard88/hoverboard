b4aca038170af8db99339e09b617e19993db34b7
Matt Biersdorf <matt@hoverboardstudios.com>
2018-04-20 16:32:53 -0700

    Post-css mixin

--- wp-content/themes/starter-theme/postcss.config.js
+++ wp-content/themes/starter-theme/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('./tasks/postcss-module-import'),
     require('precss'),
+    require('postcss-mixins'),
     require('postcss-import'),
     require('postcss-sorting'),
     require('postcss-cssnext')({
