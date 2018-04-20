29143ebe43244fc8e588486288b0f0a477a0e235
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-04-20 12:58:22 -0700

    Adding Uglify for js

--- wp-content/themes/starter-theme/webpack.prod.js
+++ wp-content/themes/starter-theme/webpack.prod.js
@@ -1,8 +1,12 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(common, {
   entry: {
     bundle: './js/main.js'
-  }
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
 })
