607eed9009556afadbb61c84adfbead7aa964a52
Matt Biersdorf <matt@hoverboardstudios.com>
2018-04-20 10:05:56 -0700

    Resolved linting errors

--- wp-content/themes/starter-theme/src/views/mobile-menu/mobile-menu.js
+++ wp-content/themes/starter-theme/src/views/mobile-menu/mobile-menu.js
@@ -1,10 +1,16 @@
-(function () {
-  document.querySelector('.mobile-toggle').addEventListener('click', (e) => toggleBodyClass(e))
-  document.querySelector('.mobile-menu__toggle').addEventListener('click', (e) => toggleBodyClass(e))
-  document.querySelector('.mobile-menu__overlay').addEventListener('click', (e) => toggleBodyClass(e))
+(function() {
+  document
+    .querySelector('.mobile-toggle')
+    .addEventListener('click', e => toggleBodyClass(e));
+  document
+    .querySelector('.mobile-menu__toggle')
+    .addEventListener('click', e => toggleBodyClass(e));
+  document
+    .querySelector('.mobile-menu__overlay')
+    .addEventListener('click', e => toggleBodyClass(e));
 
-  function toggleBodyClass (event) {
-    event.preventDefault()
-    document.querySelector('body').classList.toggle('js-mobile-menu-active')
+  function toggleBodyClass(event) {
+    event.preventDefault();
+    document.querySelector('body').classList.toggle('js-mobile-menu-active');
   }
-}())
+})();
