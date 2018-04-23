74e189d2e63175736a6d5079e91b1875b49057c1
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-04-23 14:12:33 -0700

    Sourcemaps now work, only in Dev

--- wp-content/themes/starter-theme/webpack.prod.js
+++ wp-content/themes/starter-theme/webpack.prod.js
@@ -1,10 +1,29 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = merge(common, {
   entry: {
     bundle: './js/main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'css-loader',
+          use: [
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: false,
+              },
+            },
+          ],
+        }),
+      },
+    ],
   },
   plugins: [new UglifyJSPlugin()],
 });
