c5a39bacd3e219af433543b2f63ffa513d220ac2
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-04-25 11:22:31 -0700

    Adding accordion

---
+++ wp-content/themes/starter-theme/src/views/accordion/accordion.js
@@ -0,0 +1,20 @@
+// TODO: Doesn't work if more than one accordion is on the page.
+const accordion = document.querySelector('.accordion');
+const accordionItems = accordion.querySelectorAll('.accordion__item');
+const accordionButtons = accordion.querySelectorAll('.accordion__button');
+
+// If you want the first open by default
+accordionItems[0].classList.add('accordion__item--active');
+
+accordionButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    event.preventDefault();
+
+    accordionItems.forEach(item => {
+      item.classList.remove('accordion__item--active');
+    });
+
+    // add active class to button
+    button.parentNode.classList.add('accordion__item--active');
+  });
+});
