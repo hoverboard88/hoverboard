c40fc6bddf2ea7d4d57bcd1689744b7e75eb2307
Matt Biersdorf <matt@hoverboardstudios.com>
2018-04-26 16:40:06 -0500

    Switch forEach() to map() since a value is getting returned

--- wp-content/themes/starter-theme/src/js/main.js
+++ wp-content/themes/starter-theme/src/js/main.js
@@ -3,7 +3,7 @@ import '../css/main.css'; // …so webpack can bundle css
 // Initialize Modules
 const modules = document.querySelectorAll('[data-init-js]');
 
-Array.from(modules).forEach(module => {
+Array.from(modules).map(module => {
   const moduleName = module.dataset.initJs;
   const moduleOptions = module.dataset.optionsJs;
   const moduleLoad = require('../views/' +
