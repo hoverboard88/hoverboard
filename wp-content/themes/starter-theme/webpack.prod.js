6680b311627da4c6805940364141197596c72833
Matt Biersdorf <matt@hoverboardstudios.com>
2018-04-27 11:23:32 -0500

    Added babel-polyfill for Array.from(), Object.assign(), etc...

--- wp-content/themes/starter-theme/webpack.prod.js
+++ wp-content/themes/starter-theme/webpack.prod.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = merge(common, {
   entry: {
-    bundle: './js/main.js',
+    bundle: ['babel-polyfill', './js/main.js'],
   },
   module: {
     rules: [
