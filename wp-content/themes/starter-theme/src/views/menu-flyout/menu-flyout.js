d531a3b185509b0cd58f1b8bbf6c4fdcf807eb86
Matt Biersdorf <matt@hoverboardstudios.com>
2018-05-07 14:24:50 -0500

    Completed JavaScript for hover functionality

--- wp-content/themes/starter-theme/src/views/menu-flyout/menu-flyout.js
+++ wp-content/themes/starter-theme/src/views/menu-flyout/menu-flyout.js
@@ -7,17 +7,49 @@ class MenuFlyout {
    * Creates an instance of Accordion.
    * @param {any} element HTML element of the accordion
    * @param {string} [options='{}'] Options provided by data-options-js data attribute
-   * @memberof MenuFlyout
    */
   constructor(element, options = '{}') {
     this.element = element;
     this.options = JSON.parse(options);
   }
   /**
+   * @type {Object} NodeList of menu items with children.
+   */
+  get menuItems() {
+    return this.element.querySelectorAll('.menu-item-has-children');
+  }
+  /**
+   * @param {Event} event listen event on mouseenter.
+   */
+  open(event) {
+    const firstChildLink = this.querySelector('.header-menu__link');
+    if (firstChildLink) {
+      this.classList.add('menu-item-is-open');
+      firstChildLink.setAttribute('aria-expanded', 'true');
+    }
+    event.preventDefault();
+    return false;
+  }
+  /**
+   * @param {Event} event listen event on mouseleave.
+   */
+  close(event) {
+    const firstChildLink = this.querySelector('.header-menu__link');
+    if (firstChildLink) {
+      this.classList.remove('menu-item-is-open');
+      firstChildLink.setAttribute('aria-expanded', 'false');
+    }
+    event.preventDefault();
+    return false;
+  }
+  /**
    * Initialize.
    */
   init() {
-    console.log('initalize flyouts...');
+    this.menuItems.forEach((element, index) => {
+      element.addEventListener('mouseenter', this.open);
+      element.addEventListener('mouseleave', this.close);
+    });
   }
 }
 
