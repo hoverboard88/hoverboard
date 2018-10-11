b30e9366182572f3bbc55d7755d33c38b451c7fc
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-10-11 16:29:43 -0700

    Adding starter class to twig/css

--- wp-content/themes/starter-theme/src/views/createModule.sh
+++ wp-content/themes/starter-theme/src/views/createModule.sh
@@ -5,3 +5,6 @@ printf "Creating Module: \"${1}\"";
 mkdir ${1}
 touch ${1}/${1}.css
 touch ${1}/${1}.twig
+
+printf "<div class=\"${1}\">\n  \n</div>" >> ${1}/${1}.twig
+printf ".${1} {\n\n}" >> ${1}/${1}.css
