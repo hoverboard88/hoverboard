3a291a16a134b56a398d6a24224b5b7c8378911d
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-10-23 16:55:26 -0700

    Making all non-declared functions public: https://github.com/timber/starter-theme/commit/13e3465c6d5e2dba282967db647dca10ad1fbd39

--- wp-content/themes/starter-theme/functions.php
+++ wp-content/themes/starter-theme/functions.php
@@ -40,7 +40,7 @@ Timber::$autoescape = false;
  */
 class StarterSite extends TimberSite {
 
-  function __construct() {
+  public function __construct() {
 
     $this->theme_uri = get_template_directory_uri();
     $this->menus = [
@@ -112,7 +112,7 @@ class StarterSite extends TimberSite {
     add_theme_support( 'menus' );
    }
 
-  function register_post_types() {
+  public function register_post_types() {
     // Example
     // $this->add_post_type([
     //   'name' => 'members',
@@ -125,11 +125,11 @@ class StarterSite extends TimberSite {
     // ]);
   }
 
-  function register_taxonomies() {
+  public function register_taxonomies() {
     // Use `$this->add_taxonomy()` to register taxonomies
   }
 
-  function register_menus() {
+  public function register_menus() {
 
     // Loop all menus from above and register
     foreach ($this->menus as $menu) {
@@ -231,12 +231,12 @@ class StarterSite extends TimberSite {
     register_taxonomy( $args['name'], $post_type, $tax );
   }
 
-  function mce_buttons_2( $buttons ) {
+  public function mce_buttons_2( $buttons ) {
     array_unshift( $buttons, 'styleselect' );
     return $buttons;
   }
 
-  function mce_button_styles( $init_array ) {
+  public function mce_button_styles( $init_array ) {
     // Define the style_formats array
     $style_formats = array(
       // Each array child is a format with it's own settings
@@ -268,7 +268,7 @@ class StarterSite extends TimberSite {
     <!-- Favicon HTML -->
   <?php }
 
-  function enqueue_scripts_styles() {
+  public function enqueue_scripts_styles() {
     $dev_suffix = IS_DEV ? '.dev' : '';
 
     wp_enqueue_style( 'hb_dev_css', get_template_directory_uri() . "/dist/css/bundle$dev_suffix.css", false, filemtime( get_stylesheet_directory() . "/dist/css/bundle$dev_suffix.css" ));
@@ -276,7 +276,7 @@ class StarterSite extends TimberSite {
   }
 
   // Add variables to templates
-  function add_to_context( $context ) {
+  public function add_to_context( $context ) {
 
     // loop through menus above and pass to template
     foreach ($this->menus as $menu) {
@@ -295,12 +295,12 @@ class StarterSite extends TimberSite {
     return $context;
   }
 
-  function svg( $filename ) {
+  public function svg( $filename ) {
     $file = get_stylesheet_directory() . '/src/img/' . $filename . '.svg';
     return file_exists($file) ? file_get_contents($file) : false;
   }
 
-  function srcset( $image ) {
+  public function srcset( $image ) {
     if ( gettype($image) === 'object' ) {
       return wp_get_attachment_image_srcset($image->ID, 'large');
     }
@@ -313,22 +313,22 @@ class StarterSite extends TimberSite {
     }
   }
 
-  function targetAttr( $target ) {
+  public function targetAttr( $target ) {
     if ($target) {
       return 'target="foo' . $target . '"';
     }
   }
 
-  function permalink( $id ) {
+  public function permalink( $id ) {
     return get_permalink($id);
   }
 
-  function myfoo( $text ) {
+  public function myfoo( $text ) {
     $text .= ' bar!';
     return $text;
   }
 
-  function add_to_twig( $twig ) {
+  public function add_to_twig( $twig ) {
     /* this is where you can add your own functions to twig */
     $twig->addExtension( new Twig_Extension_StringLoader() );
     $twig->addFilter( new Twig_SimpleFilter( 'svg', array( $this, 'svg' ) ) );
@@ -341,7 +341,7 @@ class StarterSite extends TimberSite {
     return $twig;
   }
 
-  function blocks_editor_enqueue() {
+  public function blocks_editor_enqueue() {
     $dev_suffix = IS_DEV ? '.dev' : '';
 
     wp_enqueue_script( 'hb_blocks_js', get_template_directory_uri() . "/dist/js/blocks$dev_suffix.js", array( 'wp-blocks', 'wp-i18n', 'wp-element' ), filemtime( get_stylesheet_directory() . "/dist/js/blocks$dev_suffix.js" ), true);
@@ -349,7 +349,7 @@ class StarterSite extends TimberSite {
     wp_enqueue_style( 'hb_blocks_editor_css', get_template_directory_uri() . "/dist/css/editor$dev_suffix.css", false, filemtime( get_stylesheet_directory() . "/dist/css/editor$dev_suffix.css" ));
   }
 
-  function blocks_enqueue() {
+  public function blocks_enqueue() {
     $dev_suffix = IS_DEV ? '.dev' : '';
 
     wp_enqueue_style( 'hb_blocks_css', get_template_directory_uri() . "/dist/css/blocks$dev_suffix.css", false, filemtime( get_stylesheet_directory() . "/dist/css/blocks$dev_suffix.css" ));
