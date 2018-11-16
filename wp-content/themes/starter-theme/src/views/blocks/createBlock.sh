4c5f9ad531e523a67a17d2f59dba84a75e109265
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-11-16 09:49:52 -0800

    Adding README/block shell script

---
+++ wp-content/themes/starter-theme/src/views/blocks/createBlock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+printf "Creating Block: \"${1}\"";
+
+mkdir ${1}
+touch ${1}/${1}.css
+touch ${1}/${1}.twig
+
+printf "<div class=\"${1} align{{align_style}}\">\n\n</div>" >> ${1}/${1}.twig
+printf ".${1} {\n  /* Styles here */\n}" >> ${1}/${1}.css
+
+# TODO: Add `$this->register_block` to functions.php? Might have to break it into a separate file so you can append to it.
