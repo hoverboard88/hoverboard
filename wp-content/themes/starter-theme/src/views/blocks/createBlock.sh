4a9599c7b4305a6107a15e5ae881b984812fe813
Ryan Tvenge <ryan@hoverboardstudios.com>
2019-02-25 11:35:41 -0800

    Adding search and replace

--- wp-content/themes/starter-theme/src/views/blocks/createBlock.sh
+++ wp-content/themes/starter-theme/src/views/blocks/createBlock.sh
@@ -5,7 +5,8 @@ printf "Creating Block: \"${1}\"";
 mkdir ${1}
 cp ../_deactivated/block-boilerplate/block-boilerplate.jsx ${1}/${1}.jsx
 
-# TODO: Search and replace boilerplate jsx file
+sed -i -e "s/BLOCK_NAME/${1}/g" ${1}/${1}.jsx
+
 printf ".${1} {\n  /* Front-end styles here */\n}" >> ${1}/${1}.view.css
 printf "/* Back-end (block editor) styles here. These should be pretty minimal. */" >> ${1}/${1}.editor.css
 
