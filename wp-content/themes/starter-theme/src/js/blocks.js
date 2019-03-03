b8000607e635084f8c8574a4b80c8d87a01bc6d4
Matt Biersdorf <matt@hoverboardstudios.com>
2019-03-03 16:28:05 -0600

    Added accordion and accordion item to blocks.js

--- wp-content/themes/starter-theme/src/js/blocks.js
+++ wp-content/themes/starter-theme/src/js/blocks.js
@@ -1,2 +1,4 @@
+import '../views/blocks/accordion/accordion.jsx';
+import '../views/blocks/accordion/accordion-item.jsx';
 import '../views/blocks/hero/hero.jsx';
 import '../views/blocks/slider/slider.jsx';
