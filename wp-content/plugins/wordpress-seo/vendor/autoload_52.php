59b75969c10b376e54e13c80cfe194b7cf18717c
Ryan Tvenge <ryan@hoverboardstudios.com>
2019-06-14 17:26:52 +0000

    Update core, plugins and themes

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1855f22676ba8649e856004038289e54::getLoader();
+return ComposerAutoloaderInit3df97fd6b08f820fdf0fa0d030844312::getLoader();
