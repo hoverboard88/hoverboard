99875c108a6c036e28aa5ec928f0e7544e45bea3
Matt Biersdorf <matt@hoverboardstudios.com>
2019-07-23 21:38:49 +0000

    Update core, plugins and themes

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit3df97fd6b08f820fdf0fa0d030844312::getLoader();
+return ComposerAutoloaderInit2e4c2a1baaeaf879a0e6b3d55e04b5d2::getLoader();
