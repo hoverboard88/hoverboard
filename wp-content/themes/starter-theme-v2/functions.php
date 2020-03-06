a97badd476745f0dc6f7d7325352fd23d848d71d
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-03-06 15:47:14 -0800

    Removing customizer php

--- wp-content/themes/starter-theme-v2/functions.php
+++ wp-content/themes/starter-theme-v2/functions.php
@@ -3,7 +3,6 @@ require get_template_directory() . '/inc/setup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/menu.php';
 require get_template_directory() . '/inc/widget.php';
-require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/image.php';
 require get_template_directory() . '/inc/post.php';
 require get_template_directory() . '/inc/taxonomy.php';
