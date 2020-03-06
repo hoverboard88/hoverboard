7a71ae6de6190595c967f27243b022172d841b5f
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-03-06 15:50:43 -0800

    Removing favicon, can just use WP's core favicon functionality

--- wp-content/themes/starter-theme-v2/functions.php
+++ wp-content/themes/starter-theme-v2/functions.php
@@ -7,7 +7,6 @@ require get_template_directory() . '/inc/image.php';
 require get_template_directory() . '/inc/post.php';
 require get_template_directory() . '/inc/taxonomy.php';
 require get_template_directory() . '/inc/shortcodes.php';
-require get_template_directory() . '/inc/favicon.php';
 require get_template_directory() . '/inc/module.php';
 require get_template_directory() . '/inc/acf.php';
 require get_template_directory() . '/inc/cptui.php';
--- wp-content/themes/starter-theme-v2/inc/favicon.php
+++
@@ -1,6 +0,0 @@
-<?php
-function hb_wp_head() { ?>
-  <!-- Favicon HTML -->
-  <!-- TODO: can we make base html for this? -->
-<?php }
-add_action( 'wp_head', 'hb_wp_head' );
