b60f0843e0825daae7953460e6928107da5254a0
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-03-26 10:25:33 -0700

    Tweaking script

--- wp-content/themes/starter-theme-v2/bash_scripts/createBlock.sh
+++ wp-content/themes/starter-theme-v2/bash_scripts/createBlock.sh
@@ -11,4 +11,6 @@ touch modules/blocks/${1}/${1}.php
 
 printf "<?php\n/**\n *	${1} Block\n *\n * @package Hoverboard\n */\n\n?>\n<div class=\"${1}-block align<?php echo esc_html( \$align_style ); ?>\">\n	<?php\n	the_module(\n		'${1}',\n		array(\n			'fields' => \$fields,\n		)\n	);\n	?>\n</div>\n" >> modules/blocks/${1}/${1}.php
 
-printf "hb_register_block( \n	array( \n		'name'        => '${1}', \n		'title'       => __( '${1}' ), \n		'description' => __( 'A slider to move content from one section to another.' ), \n		'category'    => 'layout', \n		'icon'        => 'list-view', \n		'keywords'    => array(), \n	) \n);\n" >> inc/blocks.php
+printf "\nhb_register_block(\n	array(\n		'name'        => '${1}',\n		'title'       => __( '${1}' ),\n		'description' => __( 'DESCRIPTION OF BLOCK.' ),\n		'category'    => 'layout',\n		'icon'        => 'list-view',\n		'keywords'    => array(),\n	)\n);\n" >> inc/blocks.php
+
+echo "${1} Block created. You still have to create an ACF Field Group for it in the WordPress admin."
