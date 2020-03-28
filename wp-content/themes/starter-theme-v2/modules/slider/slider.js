04d61fdadfda15469abce18d5684f02780173490
Matt Biersdorf <matt@hoverboardstudios.com>
2020-03-28 15:55:51 -0500

    modules: modified slider

--- wp-content/themes/starter-theme-v2/modules/slider/slider.js
+++ wp-content/themes/starter-theme-v2/modules/slider/slider.js
@@ -1,50 +1,11 @@
-import Glide from '@glidejs/glide';
-import {Controls, Breakpoints} from '@glidejs/glide/dist/glide.modular.esm';
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
 
-/**
- * Slider
- * @class Slider
- */
-class Slider {
-	/**
-	 * Creates an instance of Accordion.
-	 * @param {any} element HTML element of the accordion
-	 * @param {string} [options='{}'] Options provided by data-options-js data attribute
-	 * @memberof Slider
-	 */
+export class Slider {
 	constructor(element, options = '{}') {
 		this.element = element;
 		this.options = JSON.parse(options);
-		this.cssClass = this.element.classList[0];
 	}
-	/**
-	 * Initialize.
-	 */
 	init() {
-		console.log(Glide);
-		console.log(Controls);
-		console.log(Breakpoints);
-		console.log(this.element);
-		console.log('foo bar');
-
-		return new Glide(this.element, {
-			type: 'carousel',
-			classes: {
-				direction: {
-					ltr: `${this.cssClass}--ltr`,
-					rtl: `${this.cssClass}--rtl`,
-				},
-				slider: `${this.cssClass}--slider`,
-				carousel: `${this.cssClass}--carousel`,
-				swipeable: `${this.cssClass}--swipeable`,
-				dragging: `${this.cssClass}--dragging`,
-				cloneSlide: `${this.cssClass}__slide--clone`,
-				activeNav: `${this.cssClass}__bullet--active`,
-				activeSlide: `${this.cssClass}__slide--active`,
-				disabledArrow: `${this.cssClass}__arrow--disabled`,
-			},
-		}).mount({Controls, Breakpoints});
+		return new Glide(this.element).mount({ Controls, Breakpoints });
 	}
 }
-
-export {Slider};
--- wp-content/themes/starter-theme-v2/modules/slider/slider.php
+++ wp-content/themes/starter-theme-v2/modules/slider/slider.php
@@ -1,58 +1,35 @@
-<?php
-/**
- * Slider
- *
- * @package Hoverboard
- */
-
-?>
-
-<section class="slider" data-init-js="slider">
-	<div class="js-slider">
-		<div data-glide-el="track" class="slider__track">
-			<ul data-glide-el="slides" class="slider__slides">
+<section class="slider" data-init-js="Slider">
+	<div class="glide">
+		<div class="glide__track" data-glide-el="track">
+			<ul class="glide__slides">
 				<?php foreach ( $fields['slides'] as $slide ) : ?>
-					<li class="slider__slide">
-						<h3 class="slider__title">
+					<li class="glide__slide">
+						<h3 class="glide__title">
 							<?php echo esc_html( $slide['title'] ); ?>
 						</h3>
 
 						<?php
-						the_module(
-							'image',
-							array(
-								'image_id' => $slide['image']['ID'],
-								'size'     => 'large',
-								'loading'  => 'lazy',
-							)
-						);
-						?>
+							the_module( 'image',
+								array(
+									'image_id' => $slide['image']['ID'],
+									'size'     => 'large',
+									'loading'  => 'lazy',
+								)
+							);
+							?>
 
-						<div class="slider__text">
-							<?php echo wp_kses_post( $slide['text'] ); ?>
-						</div>
+							<div class="slider__text">
+								<?php echo wp_kses_post( $slide['text'] ); ?>
+							</div>
 					</li>
 				<?php endforeach; ?>
 			</ul>
 		</div>
 
-		<?php if ( count( $fields['slides'] ) > 1 ) : ?>
-			<div class="slider__arrows" data-glide-el="controls">
-				<button class="slider__arrow slider__arrow--prev" data-glide-dir="&lt;">
-					<?php hb_the_svg( 'arrow-left' ); ?>
-				</button>
-				<button class="slider__arrow slider__arrow--next" data-glide-dir="&gt;">
-					<?php hb_the_svg( 'arrow-right' ); ?>
-				</button>
-			</div>
-
-			<div class="slider__bullets" data-glide-el="controls[nav]">
-				<?php foreach ( $fields['slides'] as $key => $slide ) : ?>
-					<button class="slider__bullet" data-glide-dir="={{loop.index - 1}}">
-						<?php echo esc_html( $key ); ?>
-					</button>
-				<?php endforeach; ?>
-			</div>
-		<?php endif; ?>
+		<div class="glide__bullets" data-glide-el="controls[nav]">
+			<button class="glide__bullet" data-glide-dir="=0"></button>
+			<button class="glide__bullet" data-glide-dir="=1"></button>
+			<button class="glide__bullet" data-glide-dir="=2"></button>
+		</div>
 	</div>
 </section>
