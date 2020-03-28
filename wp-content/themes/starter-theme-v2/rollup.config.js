956efa164c70f72f4f63fd0e579055dc72404995
Matt Biersdorf <matt@hoverboardstudios.com>
2020-03-28 15:54:26 -0500

    build: removed dynamic import; used static import

--- wp-content/themes/starter-theme-v2/rollup.config.js
+++ wp-content/themes/starter-theme-v2/rollup.config.js
@@ -1,13 +1,17 @@
 import babel from 'rollup-plugin-babel';
+import browsersync from 'rollup-plugin-browsersync';
 import copy from 'rollup-plugin-copy';
 import postcss from 'rollup-plugin-postcss';
 import postcssNested from 'postcss-nested';
 import postcssCustomMedia from 'postcss-custom-media';
-import browsersync from 'rollup-plugin-browsersync';
-import {plugin as globImport} from 'rollup-plugin-glob-import';
-// TODO: Is this needed?
 import resolve from '@rollup/plugin-node-resolve';
-import multi from '@rollup/plugin-multi-entry';
+import {plugin as globImport} from 'rollup-plugin-glob-import';
+
+const plugins = [
+	babel({ exclude: 'node_modules/**' }),
+	globImport(),
+	resolve()
+];
 
 export default [
 	{
@@ -30,35 +34,15 @@ export default [
 					postcssNested(),
 				],
 				sourceMap: true,
-			}),
-			browsersync({
-				host: 'https://hoverboard-custom-upstream.lndo.site',
-				port: 3000,
-				proxy: 'https://hoverboard-custom-upstream.lndo.site',
-				files: ['**/*.php', '**/*.css'],
-				open: false,
-			}),
+			})
 		],
-	},
-	{
-		input: 'modules/**/*.js',
-		output: {
-			dir: 'assets/js',
-			format: 'esm',
-			name: 'modules',
-		},
-		plugins: [multi(), resolve(), babel()],
 	},
 	{
 		input: 'src/js/main.js',
 		output: {
-			dir: 'assets/js',
-			format: 'esm',
+			file: 'assets/js/main.js',
+			format: 'es',
 		},
-		plugins: [
-			babel({
-				exclude: 'node_modules/**',
-			}),
-		],
-	},
+		plugins: plugins
+	}
 ];
