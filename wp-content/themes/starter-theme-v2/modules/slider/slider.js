17ec088f9c388160bc9248bf8bdb994715d1aedb
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-03-30 08:57:40 -0700

    Changing slider classes back to slider

--- wp-content/themes/starter-theme-v2/modules/slider/slider.js
+++ wp-content/themes/starter-theme-v2/modules/slider/slider.js
@@ -1,11 +1,32 @@
-import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
+import Glide, {
+	Controls,
+	Breakpoints,
+} from '@glidejs/glide/dist/glide.modular.esm';
 
 export class Slider {
 	constructor(element, options = '{}') {
 		this.element = element;
 		this.options = JSON.parse(options);
+		this.cssClass = this.element.classList[0];
 	}
 	init() {
-		return new Glide(this.element).mount({ Controls, Breakpoints });
+		console.log('slider initied');
+
+		return new Glide(this.element, {
+			classes: {
+				direction: {
+					ltr: `${this.cssClass}--ltr`,
+					rtl: `${this.cssClass}--rtl`,
+				},
+				slider: `${this.cssClass}--slider`,
+				carousel: `${this.cssClass}--carousel`,
+				swipeable: `${this.cssClass}--swipeable`,
+				dragging: `${this.cssClass}--dragging`,
+				cloneSlide: `${this.cssClass}__slide--clone`,
+				activeNav: `${this.cssClass}__bullet--active`,
+				activeSlide: `${this.cssClass}__slide--active`,
+				disabledArrow: `${this.cssClass}__arrow--disabled`,
+			},
+		}).mount({ Controls, Breakpoints });
 	}
 }
--- wp-content/themes/starter-theme-v2/modules/slider/slider.php
+++ wp-content/themes/starter-theme-v2/modules/slider/slider.php
@@ -10,38 +10,36 @@
 
 ?>
 <section class="slider" data-init-js="Slider">
-	<div class="glide">
-		<div class="glide__track" data-glide-el="track">
-			<ul class="glide__slides">
-				<?php foreach ( $fields['slides'] as $slide ) : ?>
-					<li class="glide__slide">
-						<h3 class="glide__title">
-							<?php echo esc_html( $slide['title'] ); ?>
-						</h3>
+	<div class="slider__track" data-glide-el="track">
+		<ul class="slider__slides">
+			<?php foreach ( $fields['slides'] as $slide ) : ?>
+				<li class="slider__slide">
+					<h3 class="slider__title">
+						<?php echo esc_html( $slide['title'] ); ?>
+					</h3>
 
-						<?php
-						the_module(
-							'image',
-							array(
-								'image_id' => $slide['image']['ID'],
-								'size'     => 'large',
-								'loading'  => 'lazy',
-							)
-						);
-						?>
+					<?php
+					the_module(
+						'image',
+						array(
+							'image_id' => $slide['image']['ID'],
+							'size'     => 'large',
+							'loading'  => 'lazy',
+						)
+					);
+					?>
 
-						<div class="slider__text">
-							<?php echo wp_kses_post( $slide['text'] ); ?>
-						</div>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
-
-		<div class="glide__bullets" data-glide-el="controls[nav]">
-			<?php foreach ( $fields['slides'] as $index => $slide ) : ?>
-				<button class="glide__bullet" data-glide-dir="=<?php echo esc_html( $index ); ?>"></button>
+					<div class="slider__text">
+						<?php echo wp_kses_post( $slide['text'] ); ?>
+					</div>
+				</li>
 			<?php endforeach; ?>
-		</div>
+		</ul>
+	</div>
+
+	<div class="slider__bullets" data-glide-el="controls[nav]">
+		<?php foreach ( $fields['slides'] as $index => $slide ) : ?>
+			<button class="slider__bullet" data-glide-dir="=<?php echo esc_html( $index ); ?>"></button>
+		<?php endforeach; ?>
 	</div>
 </section>
