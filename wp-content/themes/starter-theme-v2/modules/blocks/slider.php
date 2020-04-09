10d8adf15b4aa9d1ca352da733a0cebd81ef2d88
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-04-09 14:11:38 -0700

    Removing  variable on blocks

--- wp-content/themes/starter-theme-v2/modules/blocks/slider.php
+++ wp-content/themes/starter-theme-v2/modules/blocks/slider.php
@@ -7,5 +7,10 @@
 
 ?>
 <div class="slider-block align<?php echo esc_html( $align_style ); ?>">
-	<?php the_module( 'slider', array( 'fields' => $fields ) ); ?>
+	<?php
+	the_module(
+		'slider',
+		$fields,
+	);
+	?>
 </div>
