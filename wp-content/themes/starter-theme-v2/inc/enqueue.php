aba4290583d7d7928844ffdce480e6c6432eb8c0
Matt Biersdorf <matt@hoverboardstudios.com>
2020-04-18 09:11:15 -0500

    backend: wp_enqueue_script phpcs issue

--- wp-content/themes/starter-theme-v2/inc/enqueue.php
+++ wp-content/themes/starter-theme-v2/inc/enqueue.php
@@ -11,7 +11,7 @@
 function hb_enqueue_scripts() {
 	wp_enqueue_style( 'google', '//fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&display=swap', array(), '1.0.0' );
 	wp_enqueue_style( 'theme', get_template_directory_uri() . '/assets/css/main.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/main.css' ) );
-	wp_enqueue_script( 'glidejs', '//unpkg.com/@glidejs/glide@3.4.1/dist/glide.min.js', array(), null, true );
+	wp_enqueue_script( 'glidejs', '//unpkg.com/@glidejs/glide@3.4.1/dist/glide.min.js', array(), false, true );
 	wp_enqueue_script( 'theme', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/assets/js/main.js' ), true );
 }
 
