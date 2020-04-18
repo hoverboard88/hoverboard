d96db1d2a54d58c35cec812db87ebe7325b2f750
Matt Biersdorf <matt@hoverboardstudios.com>
2020-04-18 09:02:12 -0500

    build: js uglify success; css minify not

--- wp-content/themes/starter-theme-v2/gulpfile.js
+++ wp-content/themes/starter-theme-v2/gulpfile.js
@@ -1,61 +1,28 @@
-const { parallel, src, dest, watch, series } = require('gulp');
-const postcss = require('gulp-postcss');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify');
-// TODO: If you don't use, remove from package.json and any other unused modules
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+// TODO: gulp-uglify doesn't play well with postcss; recommends using cssnano
+// cssnano() minified the code but broke parts of the design.
+// .pipe(postcss([cssnano()]))
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const { dest, parallel, src, watch } = require('gulp');
+const postcss = require('gulp-postcss');
 const svgo = require('gulp-svgo');
+const uglify = require('gulp-uglify');
 
-// const webpack = require('webpack-stream');
-
-const glob = {
-	css: ['./src/css/global/*.css', './modules/**/*.css'],
-	js: ['modules/**/*.js', 'src/js/modules.js', 'src/js/animate.js'],
-	php: ['**/*.php'],
-	image: ['src/images/*'],
-};
-
-// TODO: Can this concatenate all the modules/**.*.js files?
-function mainJS() {
-	return (
-		src(glob.js)
-			.pipe(sourcemaps.init())
-			.pipe(concat('main.js'))
-			// TODO: import/exports not working like in Rollup.
-			// .pipe(
-			// 	webpack({
-			// 		module: {
-			// 			rules: [{ test: /\.js$/ }],
-			// 		},
-			// 		output: {
-			// 			libraryTarget: 'umd',
-			// 		},
-			// 	})
-			// )
-			.pipe(
-				babel({
-					presets: ['@babel/preset-env'],
-				})
-			)
-			.pipe(uglify())
-			.pipe(sourcemaps.write('.'))
-			.pipe(dest('./assets/js'))
-	);
+function js() {
+	return src(['src/js/**/*.js', 'modules/**/*.js'])
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(dest('./assets/js', { sourcemaps: true }));
 }
 
-function mainCSS() {
-	return (
-		src(glob.css)
-			.pipe(sourcemaps.init())
-			.pipe(concat('main.css'))
-			.pipe(postcss()) // Config in postcss.config.js
-			// TODO: Error's on CSS
-			// .pipe(uglify())
-			.pipe(sourcemaps.write('.'))
-			.pipe(dest('./assets/css'))
-	);
+function css() {
+	return src(['./src/css/global/*.css', './modules/**/*.css'])
+		.pipe(concat('main.css'))
+		.pipe(postcss())
+		.pipe(dest('./assets/css', { sourcemaps: true }));
 }
 
 function connectSync() {
@@ -70,10 +37,16 @@ function connectSync() {
 function watchFiles() {
 	connectSync();
 
-	watch(glob.css, parallel([mainCSS, browserSyncReload]));
-	watch(glob.js, parallel([mainJS, browserSyncReload]));
-	watch(glob.php, parallel([browserSyncReload]));
-	watch(glob.image, parallel([images]));
+	watch(
+		['./src/css/global/*.css', './modules/**/*.css'],
+		parallel([css, browserSyncReload])
+	);
+	watch(
+		['modules/**/*.js', 'src/js/modules.js', 'src/js/animate.js'],
+		parallel([js, browserSyncReload])
+	);
+	watch(['**/*.php'], parallel([browserSyncReload]));
+	watch(['src/images/*'], parallel([images]));
 }
 
 function browserSyncReload(done) {
@@ -82,8 +55,8 @@ function browserSyncReload(done) {
 }
 
 function images() {
-	return src(glob.image).pipe(svgo()).pipe(dest('./assets/images'));
+	return src(['src/images/*']).pipe(svgo()).pipe(dest('./assets/images'));
 }
 
-exports.default = parallel([mainCSS, mainJS, images]);
-exports.watch = parallel([mainCSS, mainJS, images, watchFiles]);
+exports.default = parallel([css, js, images]);
+exports.watch = parallel([css, js, images, watchFiles]);
