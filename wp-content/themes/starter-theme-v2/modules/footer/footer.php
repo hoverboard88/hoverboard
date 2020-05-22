02d3f3ba1f8aef0007a0e9ce4ef6a712f7440201
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-05-22 15:49:49 -0700

    Fixing phpcs error

--- wp-content/themes/starter-theme-v2/modules/footer/footer.php
+++ wp-content/themes/starter-theme-v2/modules/footer/footer.php
@@ -11,16 +11,59 @@
 ?>
 <footer class="footer">
 	<div class="container">
-		<?php
-		the_module( 'menu-footer', array( 'menu_name' => 'menu_footer' ) );
+		<div class="footer__menu">
+			<?php
+			the_module(
+				'menu-footer',
+				array(
+					'menu_name' => 'menu_footer',
+				)
+			);
+			?>
+		</div>
 
-		if ( get_field( 'address', 'options' ) ) {
-			the_module( 'address', get_field( 'address', 'options' ) );
-		}
+		<?php if ( get_field( 'address', 'options' ) ) : ?>
+			<div class="footer__address">
+				<?php
+				the_module(
+					'address',
+					get_field( 'address', 'options' ),
+				);
+				?>
+			</div>
+		<?php endif; ?>
 
-		the_module( 'copyright', array( 'text' => get_field( 'fine_print', 'options' ) ) );
-		the_module( 'menu-footer-secondary', array( 'menu_name' => 'menu_footer_secondary' ) );
-		the_module( 'menu-social', array( 'social_links' => get_field( 'social_links', 'options' ) ) );
-		?>
+		<div class="footer__copyright">
+			<?php
+			the_module(
+				'copyright',
+				array(
+					'text' => get_field( 'fine_print', 'options' ),
+				)
+			);
+			?>
+		</div>
+
+		<div class="footer__menu-secondary">
+			<?php
+			the_module(
+				'menu-footer-secondary',
+				array(
+					'menu_name' => 'menu_footer_secondary',
+				)
+			);
+			?>
+		</div>
+
+		<div class="footer__social">
+			<?php
+			the_module(
+				'menu-social',
+				array(
+					'social_links' => get_field( 'social_links', 'options' ),
+				)
+			);
+			?>
+		</div>
 	</div>
 </footer>
