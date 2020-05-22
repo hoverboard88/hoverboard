24dc636b1a79d8b0478f042713329882dfb4614d
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-05-22 16:05:07 -0700

    Adding extend

--- wp-content/themes/starter-theme-v2/postcss.config.js
+++ wp-content/themes/starter-theme-v2/postcss.config.js
@@ -2,10 +2,17 @@ const postcssNested = require('postcss-nested');
 const postcssCustomMedia = require('postcss-custom-media');
 const autoprefixer = require('autoprefixer');
 const atImport = require('postcss-import');
+const extend = require('postcss-extend');
 
 module.exports = {
 	extract: true,
 	minimize: true,
-	plugins: [atImport(), postcssCustomMedia(), postcssNested(), autoprefixer()],
+	plugins: [
+		atImport(),
+		postcssCustomMedia(),
+		extend(),
+		postcssNested(),
+		autoprefixer(),
+	],
 	sourceMap: true,
 };
