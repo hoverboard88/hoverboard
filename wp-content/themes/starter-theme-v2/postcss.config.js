7617d95cd0c429bf791441f7b5e3a5d34598aeb3
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-05-29 15:47:44 -0700

    Fixing at import error

--- wp-content/themes/starter-theme-v2/postcss.config.js
+++ wp-content/themes/starter-theme-v2/postcss.config.js
@@ -1,18 +1,11 @@
 const postcssNested = require('postcss-nested');
 const postcssCustomMedia = require('postcss-custom-media');
 const autoprefixer = require('autoprefixer');
-const atImport = require('postcss-import');
 const extend = require('postcss-extend');
 
 module.exports = {
 	extract: true,
 	minimize: true,
-	plugins: [
-		atImport(),
-		postcssCustomMedia(),
-		extend(),
-		postcssNested(),
-		autoprefixer(),
-	],
+	plugins: [postcssCustomMedia(), extend(), postcssNested(), autoprefixer()],
 	sourceMap: true,
 };
