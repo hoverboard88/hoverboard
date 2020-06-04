70f16a4c95fc5bbba000d46c11d65fcebd94df9c
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-06-04 14:58:42 +0000

    Adding dev flag so only dev things are installed

--- wp-content/themes/starter-theme-v2/bin/phpcs.sh
+++ wp-content/themes/starter-theme-v2/bin/phpcs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Runs phpcs test
-composer install
+composer install --dev
 ./vendor/bin/phpcs ./*.php ./modules/**/*.php ./inc/*.php ./templates/*.php --standard=WordPress -n --exclude=WordPress.PHP.DontExtract
