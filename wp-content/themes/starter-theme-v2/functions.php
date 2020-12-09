5e3c3eb1c368b90b43c25c66a44efe21aa91c954
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-12-09 14:14:02 -0800

    Removing coded CPT's; CPTUI plugin now.

--- wp-content/themes/starter-theme-v2/functions.php
+++ wp-content/themes/starter-theme-v2/functions.php
@@ -12,7 +12,6 @@ require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/menu.php';
 require get_template_directory() . '/inc/widget.php';
 require get_template_directory() . '/inc/image.php';
-require get_template_directory() . '/inc/post.php';
 require get_template_directory() . '/inc/blocks.php';
 require get_template_directory() . '/inc/taxonomy.php';
 require get_template_directory() . '/inc/shortcodes.php';
