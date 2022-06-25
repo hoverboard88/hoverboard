40ee1745b2317b46045960a1c0392d83ff757178
Matt Biersdorf <matt@hoverboardstudios.com>
2022-06-25 16:02:06 -0500

    Added build files

---
+++ wp-content/plugins/hoverboard-blocks/build/accordion/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '44303f5e1919049e1fc0');
---
+++ wp-content/plugins/hoverboard-blocks/build/accordion/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fb0ae9ca39a8962fe31b');
