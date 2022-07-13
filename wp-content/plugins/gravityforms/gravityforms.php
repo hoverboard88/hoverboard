e839dcc0e81f48e639763fb952d58adad05142d4
Ryan Tvenge <ryan@hoverboardstudios.com>
2022-07-13 07:53:33 -0700

    Update core, plugins and themes

--- wp-content/plugins/gravityforms/gravityforms.php
+++ wp-content/plugins/gravityforms/gravityforms.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Forms
 Plugin URI: https://gravityforms.com
 Description: Easily create web forms and manage form entries within the WordPress admin.
-Version: 2.6.3.3
+Version: 2.6.4
 Requires at least: 4.0
 Requires PHP: 5.6
 Author: Gravity Forms
@@ -121,7 +121,7 @@ define( 'GF_SUPPORTED_WP_VERSION', version_compare( get_bloginfo( 'version' ), G
  *
  * @var string GF_MIN_WP_VERSION_SUPPORT_TERMS The version number
  */
-define( 'GF_MIN_WP_VERSION_SUPPORT_TERMS', '5.8' );
+define( 'GF_MIN_WP_VERSION_SUPPORT_TERMS', '5.9' );
 
 /**
  * The filesystem path of the directory that contains the plugin, includes trailing slash.
@@ -236,7 +236,7 @@ class GFForms {
 	 *
 	 * @var string $version The version number.
 	 */
-	public static $version = '2.6.3.3';
+	public static $version = '2.6.4';
 
 	/**
 	 * Handles background upgrade tasks.
