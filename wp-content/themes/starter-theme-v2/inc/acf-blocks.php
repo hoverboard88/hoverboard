e1391542a426156e5e236577bd7060953bb75936
Matt Biersdorf <matt@hoverboardstudios.com>
2023-05-10 12:27:54 -0500

    Added wrapper block

--- wp-content/themes/starter-theme-v2/inc/acf-blocks.php
+++ wp-content/themes/starter-theme-v2/inc/acf-blocks.php
@@ -13,28 +13,7 @@ function hb_register_acf_blocks() {
 	register_block_type( get_template_directory() . '/blocks/dialog' );
 	register_block_type( get_template_directory() . '/blocks/slider' );
 	register_block_type( get_template_directory() . '/blocks/team' );
-
-	// hb_register_block(
-	// 	array(
-	// 		'name'        => 'staff-members',
-	// 		'title'       => __( 'Staff Members' ),
-	// 		'description' => __( 'List staff members, optionally by group.' ),
-	// 		'category'    => 'formatting',
-	// 		'icon'        => 'groups',
-	// 		'keywords'    => array( 'groups', 'members' ),
-	// 	)
-	// );
-
-	hb_register_block(
-		array(
-			'name'        => 'wrapper',
-			'title'       => __( 'Wrapper' ),
-			'description' => __( 'Wraps block in colored background and padding.' ),
-			'category'    => 'formatting',
-			'icon'        => 'list-view',
-			'keywords'    => array( 'wrap', 'container' ),
-		)
-	);
+	register_block_type( get_template_directory() . '/blocks/wrapper' );
 }
 
 add_action( 'init', 'hb_register_acf_blocks' );
