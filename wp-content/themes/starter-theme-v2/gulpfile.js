f2d477bc834278dab5ac15d9b2390327c7933a07
Matt Biersdorf <matt@hoverboardstudios.com>
2023-05-13 12:37:23 -0500

    Refactored build to include minified files

--- wp-content/themes/starter-theme-v2/gulpfile.js
+++ wp-content/themes/starter-theme-v2/gulpfile.js
@@ -1,11 +1,11 @@
+const { dest, parallel, src, watch } = require('gulp');
 const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const cssnano = require('gulp-cssnano');
 const concat = require('gulp-concat');
-const { dest, parallel, src, watch } = require('gulp');
 const postcss = require('gulp-postcss');
-const svgo = require('gulp-svgo');
-const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify-es').default;
 
 const globs = {
 	js: ['parts/**/*.js'],
@@ -14,8 +14,15 @@ const globs = {
 	editorcss: ['./assets/css/global/variable.css', './parts/**/*.css'],
 	blocks: ['./assets/css/global/variable.css', './blocks/**/*.css'],
 	php: ['**/*.php'],
-	images: ['assets/images/*'],
 };
+
+function processCss(source, filename) {
+	return src(source)
+		.pipe(concat(filename))
+		.pipe(postcss())
+		.pipe(cssnano())
+		.pipe(dest('./assets/css', { sourcemaps: true }));
+}
 
 function js() {
 	return src(globs.js)
@@ -25,28 +32,29 @@ function js() {
 		.pipe(dest('./assets/js', { sourcemaps: true }));
 }
 
+function blocksjs() {
+	return src(globs.blocksjs)
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.pipe(uglify())
+		.pipe(
+			rename((path) => {
+				path.basename += '.min';
+				return path;
+			})
+		)
+		.pipe(dest('./blocks/', { sourcemaps: true }));
+}
+
 function css() {
-	return src(globs.css)
-		.pipe(concat('main.css'))
-		.pipe(postcss())
-		.pipe(cssnano())
-		.pipe(dest('./assets/css', { sourcemaps: true }));
+	return processCss(globs.css, 'main.css');
 }
 
 function editorcss() {
-	return src(globs.editorcss)
-		.pipe(concat('editor.css'))
-		.pipe(postcss())
-		.pipe(cssnano())
-		.pipe(dest('./assets/css', { sourcemaps: true }));
+	return processCss(globs.editorcss, 'editor.css');
 }
 
 function blocks() {
-	return src(globs.blocks)
-		.pipe(concat('blocks.css'))
-		.pipe(postcss())
-		.pipe(cssnano())
-		.pipe(dest('./assets/css', { sourcemaps: true }));
+	return processCss(globs.blocks, 'blocks.css');
 }
 
 function connectSync() {
@@ -61,12 +69,12 @@ function connectSync() {
 function watchFiles() {
 	connectSync();
 
-	watch(globs.css, parallel([css, browserSyncReload]));
-	watch(globs.editorcss, parallel([editorcss, browserSyncReload]));
-	watch(globs.blocks, parallel([blocks, browserSyncReload]));
-	watch(globs.js, parallel([js, browserSyncReload]));
-	watch(globs.blocksjs, parallel([browserSyncReload]));
-	watch(globs.php, parallel([browserSyncReload]));
+	watch(globs.css, parallel(css, browserSyncReload));
+	watch(globs.editorcss, parallel(editorcss, browserSyncReload));
+	watch(globs.blocks, parallel(blocks, browserSyncReload));
+	watch(globs.js, parallel(js, browserSyncReload));
+	watch(globs.blocksjs, browserSyncReload);
+	watch(globs.php, browserSyncReload);
 }
 
 function browserSyncReload(done) {
@@ -74,5 +82,5 @@ function browserSyncReload(done) {
 	done();
 }
 
-exports.default = parallel([css, editorcss, blocks, js]);
+exports.default = parallel([css, editorcss, blocks, blocksjs, js]);
 exports.watch = parallel([css, editorcss, blocks, js, watchFiles]);
