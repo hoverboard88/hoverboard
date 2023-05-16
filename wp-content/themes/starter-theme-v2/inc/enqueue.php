fa5d9c43b8290d4d0545fbee00631a1a53ad6326
Matt Biersdorf <matt@hoverboardstudios.com>
2023-05-16 11:07:47 -0500

    Added .min to enqueue.php

--- wp-content/themes/starter-theme-v2/inc/enqueue.php
+++ wp-content/themes/starter-theme-v2/inc/enqueue.php
@@ -10,8 +10,8 @@
  */
 function hb_enqueue_scripts() {
 	wp_enqueue_style( 'google', '//fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&display=swap', array(), '1.0.0' );
-	wp_enqueue_style( 'theme', get_template_directory_uri() . '/assets/css/main.css', array( 'google' ), filemtime( get_stylesheet_directory() . '/assets/css/main.css' ) );
-	wp_enqueue_script( 'theme', get_template_directory_uri() . '/assets/js/main.js', array(), filemtime( get_stylesheet_directory() . '/assets/js/main.js' ), true );
+	wp_enqueue_style( 'theme', get_template_directory_uri() . '/assets/css/main.min.css', array( 'google' ), filemtime( get_stylesheet_directory() . '/assets/css/main.min.css' ) );
+	wp_enqueue_script( 'theme', get_template_directory_uri() . '/assets/js/main.min.js', array(), filemtime( get_stylesheet_directory() . '/assets/js/main.min.js' ), true );
 }
 
 add_action( 'wp_enqueue_scripts', 'hb_enqueue_scripts' );
@@ -20,7 +20,7 @@ add_action( 'wp_enqueue_scripts', 'hb_enqueue_scripts' );
  * Enqueue scripts and styles for Block Editor
  */
 function hb_enqueue_block_scripts() {
-	wp_enqueue_style( 'theme-editor', get_template_directory_uri() . '/assets/css/editor.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/editor.css' ) );
+	wp_enqueue_style( 'theme-editor', get_template_directory_uri() . '/assets/css/editor.min.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/editor.min.css' ) );
 }
 
 add_action( 'enqueue_block_editor_assets', 'hb_enqueue_block_scripts' );
@@ -30,12 +30,7 @@ add_action( 'enqueue_block_editor_assets', 'hb_enqueue_block_scripts' );
  */
 function hb_enqueue_block_script() {
 	$block_types = acf_get_block_types();
-	$min         = '';
 	$deps        = array();
-
-	if ( WP_ENVIRONMENT_TYPE !== 'local' ) {
-		$min = '.min';
-	}
 
 	foreach ( $block_types as $block_type ) {
 		$block_acf_name = $block_type['name'];
@@ -47,11 +42,11 @@ function hb_enqueue_block_script() {
 			$vendor = $script_handles[1] ?? false;
 
 			if ( $vendor ) {
-				wp_enqueue_script( $vendor, get_template_directory_uri() . "/blocks/{$block_name}/vendor{$min}.js", array(), filemtime( get_stylesheet_directory() . "/blocks/{$block_name}/vendor.js" ), true );
+				wp_enqueue_script( $vendor, get_template_directory_uri() . "/blocks/{$block_name}/vendor.min.js", array(), filemtime( get_stylesheet_directory() . "/blocks/{$block_name}/vendor.js" ), true );
 				$deps[] = $vendor;
 			}
 
-			wp_register_script( $script_handles[0], get_template_directory_uri() . "/blocks/{$block_name}/{$block_name}{$min}.js", $deps, filemtime( get_stylesheet_directory() . "/blocks/{$block_name}/{$block_name}.js" ), true );
+			wp_register_script( $script_handles[0], get_template_directory_uri() . "/blocks/{$block_name}/{$block_name}.min.js", $deps, filemtime( get_stylesheet_directory() . "/blocks/{$block_name}/{$block_name}.js" ), true );
 		}
 	}
 }
