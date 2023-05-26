f87605f02f5e23ae5c8d4d3d665b3bad799aea2b
Matt Biersdorf <matt@hoverboardstudios.com>
2023-05-26 10:55:13 -0500

    Added search toggle js

--- wp-content/themes/hoverboard/parts/search-toggle/search-toggle.js
+++ wp-content/themes/hoverboard/parts/search-toggle/search-toggle.js
@@ -1,16 +1,13 @@
 (() => {
-	// const toggle = (event) => {
-	// 	event.preventDefault();
-	// 	document.querySelector('body').classList.toggle('js-body-search-toggle');
-	// 	if (document.body.classList.contains('js-body-search-toggle')) {
-	// 		document.querySelector('.search-form__input').focus();
-	// 	}
-	// };
-	// const initialize = (searchToggle) => {
-	// 	const element = searchToggle.element;
-	// 	const options = JSON.parse(searchToggle.options);
-	// 	element.addEventListener('click', toggle);
-	// };
-	// const searchToggle = hb.setup('search-toggle');
-	// searchToggle.map(initialize);
+	const toggle = (event) => {
+		event.preventDefault();
+		document.querySelector('body').classList.toggle('js-body-search-toggle');
+		if (document.body.classList.contains('js-body-search-toggle')) {
+			document.querySelector('.search-form__input').focus();
+		}
+	};
+
+	Array.from(document.querySelectorAll('.search-toggle')).map((element) => {
+		element.addEventListener('click', toggle);
+	});
 })();
