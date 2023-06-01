c1d58740ac89d7c696a631620271ed297523a488
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-06-01 10:26:34 -0500

    Adding Hero image style.

--- wp-content/themes/hoverboard/inc/image.php
+++ wp-content/themes/hoverboard/inc/image.php
@@ -25,6 +25,7 @@ function hb_the_svg( $filename ) {
  */
 function hb_register_image_sizes() {
 	add_image_size( 'card', 800, 450, true );
+	add_image_size( 'hero', 1440 );
 }
 
 add_action( 'init', 'hb_register_image_sizes' );
