f922e3352003da5dd2cdc3379a7760f629a1003a
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-06-01 15:00:34 -0500

    Fixing args and paths on partials.

--- wp-content/themes/hoverboard/parts/address/address.php
+++ wp-content/themes/hoverboard/parts/address/address.php
@@ -8,40 +8,41 @@
 ?>
 
 <div class="address">
-	<?php if ( $title ) : ?>
+	<?php if ( $args['title'] ) : ?>
 		<h3 class="address__title">
-			<?php echo esc_html( $title ); ?>
+			<?php echo esc_html( $args['title'] ); ?>
 		</h3>
 	<?php endif; ?>
 
 	<p class="address__address">
 		<span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 			<span itemprop="streetAddress">
-				<?php echo esc_html( $address_1 ); ?><br>
+				<?php echo esc_html( $args['address_1'] ); ?><br>
 
-				<?php if ( $address_2 ) : ?>
-					<?php echo esc_html( $address_2 ); ?><br>
+				<?php if ( $args['address_2'] ) : ?>
+					<?php echo esc_html( $args['address_2'] ); ?><br>
 				<?php endif; ?>
 			</span>
 
-			<span itemprop="addressLocality"><?php echo esc_html( $city ); ?></span>,
+			<span itemprop="addressLocality"><?php echo esc_html( $args['city'] ); ?></span>,
 
 			<span itemprop="addressRegion">
-				<?php echo esc_html( $state ); ?>
+				<?php echo esc_html( $args['state'] ); ?>
 			</span>
 
 			<span itemprop="postalCode">
-				<?php echo esc_html( $zip_code ); ?>
+				<?php echo esc_html( $args['zip_code'] ); ?>
 			</span>
 		</span>
-		<?php if ( $link ) : ?>
+
+		<?php if ( $args['link'] ) : ?>
 			<br>
 			<?php
 			get_template_part(
 				'parts/link/link',
 				null,
 				array(
-					'link' => $link,
+					'link' => $args['link'],
 				)
 			);
 			?>
