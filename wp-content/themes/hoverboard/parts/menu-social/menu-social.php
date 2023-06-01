0dc3e3eebe2929f4a1b6ddaad1e91d2312c288cf
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-06-01 15:07:48 -0500

    Fixing args and paths on partials.

--- wp-content/themes/hoverboard/parts/menu-social/menu-social.php
+++ wp-content/themes/hoverboard/parts/menu-social/menu-social.php
@@ -7,12 +7,12 @@
 
 ?>
 
-<?php if ( $social_links ) : ?>
+<?php if ( $args['social_links'] ) : ?>
 	<nav class="menu-social" itemscope itemtype="http://schema.org/Organization">
 		<link itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 
 		<ul class="menu-social__items">
-			<?php foreach ( $social_links as $social_link ) : ?>
+			<?php foreach ( $args['social_links'] as $social_link ) : ?>
 				<li class="menu-social__item">
 					<a itemprop="sameAs" class="menu-social__link" href="<?php echo esc_html( $social_link['link']['url'] ); ?>" target="<?php echo esc_html( $social_link['link']['target'] ); ?>">
 						<?php hb_the_svg( $social_link['type'] ); ?>
