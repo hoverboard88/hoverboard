eb1e1bb3fb4e50569f0dd981836903d4e27e9393
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-06-02 08:49:29 -0500

    Reimplimenting nav-toggle.

--- wp-content/themes/hoverboard/parts/nav-toggle/nav-toggle.js
+++ wp-content/themes/hoverboard/parts/nav-toggle/nav-toggle.js
@@ -1,13 +1,10 @@
 (() => {
-	// const toggle = (event) => {
-	// 	event.preventDefault();
-	// 	document.querySelector('body').classList.toggle('js-body-nav-toggle');
-	// };
-	// const initialize = (navToggle) => {
-	// 	const element = navToggle.element;
-	// 	const options = JSON.parse(navToggle.options);
-	// 	element.addEventListener('click', toggle);
-	// };
-	// const navToggle = hb.setup('nav-toggle');
-	// navToggle.map(initialize);
+	const toggle = (event) => {
+		event.preventDefault();
+		document.querySelector('body').classList.toggle('js-body-nav-toggle');
+	};
+
+	Array.from(document.querySelectorAll('.nav-toggle')).map((element) => {
+		element.addEventListener('click', toggle);
+	});
 })();
