8a7810472d45a066b7be93b590876e4ff466e729
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-08-09 16:55:38 -0500

    Fixing accordion to collapse; fixing arrow rotation.

--- wp-content/themes/hoverboard/blocks/accordion/accordion.js
+++ wp-content/themes/hoverboard/blocks/accordion/accordion.js
@@ -1,32 +1,37 @@
 (() => {
 	const accordions = document.querySelectorAll('[data-block="accordion"]');
+	Array.from(accordions).map((accordion) => init(accordion));
 
-	for (const accordion of accordions) {
+	function init(accordion) {
 		const options = JSON.parse(accordion.dataset.options);
-
-		if (options.open) {
+		if (options.open)
 			accordion
 				.querySelector('.js-accordion-item')
 				.classList.add('wp-block-accordion__item--active');
-		}
-
-		accordion.addEventListener('click', accordionToggle);
+		accordion.addEventListener('click', accordionToggle.bind(accordion));
 	}
 
 	function accordionToggle(event) {
 		event.preventDefault();
-		const subTitle = event.target;
-
-		const items = subTitle
-			.closest('.wp-block-accordion')
-			.querySelectorAll('.js-accordion-item');
-
-		Array.from(items).map((item) =>
-			item.classList.remove('wp-block-accordion__item--active')
+		const accordionItems = this.querySelectorAll('.js-accordion-item');
+		const activeAccordionItem = event.target.closest('.js-accordion-item');
+		const isOpen = activeAccordionItem.classList.contains(
+			'wp-block-accordion__item--active'
 		);
 
-		subTitle
-			.closest('.js-accordion-item')
-			.classList.toggle('wp-block-accordion__item--active');
+		isOpen
+			? closeAllAccordions(accordionItems)
+			: (closeAllAccordions(accordionItems),
+			  toggleAccordion(activeAccordionItem));
+	}
+
+	function closeAllAccordions(accordions) {
+		return Array.from(accordions).map((accordion) =>
+			accordion.classList.remove('wp-block-accordion__item--active')
+		);
+	}
+
+	function toggleAccordion(accordion) {
+		return accordion.classList.toggle('wp-block-accordion__item--active');
 	}
 })();
