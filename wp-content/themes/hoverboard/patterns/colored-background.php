be0c3ab27b3261ac77f52710d78ffdbb648a95bc
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-08-10 16:47:57 -0500

    Moving colors and block patterns to theme.json.

---
+++ wp-content/themes/hoverboard/patterns/colored-background.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Colored Background Section
+ * Description: A group with a background color that spans the width of the page.
+ * Categories: hoverboard
+ * Slug: hoverboard/colored-background
+ */
+?>
+<!-- wp:group {"align":"full","backgroundColor":"gray-light"} --> <div class="wp-block-group alignfull has-gray-light-background-color has-background"><!-- wp:heading --> <h2>Title</h2> <!-- /wp:heading --> <!-- wp:paragraph --> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit…</p> <!-- /wp:paragraph --></div> <!-- /wp:group -->
