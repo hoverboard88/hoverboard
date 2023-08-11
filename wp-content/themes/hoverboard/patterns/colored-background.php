53a92ac6594e9166ad0a5a31e70e406a6e434886
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-08-11 15:31:56 -0500

    Updating to use Spacing setting.

--- wp-content/themes/hoverboard/patterns/colored-background.php
+++ wp-content/themes/hoverboard/patterns/colored-background.php
@@ -6,4 +6,8 @@
  * Slug: hoverboard/colored-background
  */
 ?>
-<!-- wp:group {"align":"full","backgroundColor":"gray-light"} --> <div class="wp-block-group alignfull has-gray-light-background-color has-background"><!-- wp:heading --> <h2>Title</h2> <!-- /wp:heading --> <!-- wp:paragraph --> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit…</p> <!-- /wp:paragraph --></div> <!-- /wp:group -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"gray-light","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group alignfull has-gray-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+<!-- wp:heading --> <h2>Title</h2> <!-- /wp:heading --> <!-- wp:paragraph --> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit…</p> <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
