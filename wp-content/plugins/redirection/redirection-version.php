6e2e6e862d669571ca6ecec5bc16397ac1f2a074
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-01-15 08:51:27 -0600

    Update core, plugins and themes

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.3.10' );
-define( 'REDIRECTION_BUILD', '948a20e46d8617ec7b6cf37114495e49' );
+define( 'REDIRECTION_VERSION', '5.4.1' );
+define( 'REDIRECTION_BUILD', '4921947a93e8d2bdecfa002a29093c22' );
 define( 'REDIRECTION_MIN_WP', '5.7' );
